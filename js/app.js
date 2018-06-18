e2d28b8f44d17859f728ccf14f74d144ce4102f4
ballestero <ballesterojj@gmail.com>
2018-06-18 07:40:17 -0600

    interevalo

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+/*var bulb = document.getElementByClassName('bulbs'),
+    bulbsCaroucel = document.getElementsByClassName('bulbsC')
+    bars =document.getElementById('bars'),
+    containerProducts = document.getElementById('containerProducts');
+
+bulb.onclick = bulbOnclick;
+//bars.onclick = barsOnclick;
+//containerProducts = document.addEventListener('click', productOnclick);
+
+function bulbOnclick(){
+    bulbsCaroucel.id = 'carouselExampleIndicators'
+}
+
+$(document).ready(function() {
+    $('.carousel').carousel({
+      interval: 16000
+    })
+  });*/
